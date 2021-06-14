389e8707bb8492c9e39e72c0bdfc029bcd397b53
marcelo-thoughtworks <marcelo.maximiano@thoughtworks.com>
2021-06-14 17:35:46 -0300

    Initial commit

---
+++ precache-manifest.717fe0724e7bed028563d9a647f53e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee3f63786fcb54952684233b2503e31",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "f2c7ff52b6f8ee2d1658",
+    "url": "/frontend/static/css/main.7973b12f.chunk.css"
+  },
+  {
+    "revision": "511eed86a5564d082ab1",
+    "url": "/frontend/static/js/2.2bfd5c67.chunk.js"
+  },
+  {
+    "revision": "5350156fe1594a8a40842d365175a2b1",
+    "url": "/frontend/static/js/2.2bfd5c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c7ff52b6f8ee2d1658",
+    "url": "/frontend/static/js/main.1ba4ca81.chunk.js"
+  },
+  {
+    "revision": "747c638a0ed8091beb0a",
+    "url": "/frontend/static/js/runtime-main.12e52466.js"
+  }
+]);
